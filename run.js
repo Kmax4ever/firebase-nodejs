f650a0adee0a0e0f5986a294eb5a164056043c91
Kmax4ever <kiettuanphan90@gmail.com>
2021-06-21 14:13:02 +0700

    add update firebase document

--- run.js
+++ run.js
@@ -9,7 +9,7 @@
 //     appId: "1:710247542015:web:9538a650c4f16e2fcc30ca"
 // });
 var fireBaseAdmin = require("firebase-admin");
-const { random } = require('lodash')
+const { random, update } = require('lodash')
 var config = require("./config/key.json");
 
 fireBaseAdmin.initializeApp({
@@ -21,80 +21,63 @@ fireBaseAdmin.initializeApp({
 const db = fireBaseAdmin.firestore();
 
 
-async function get() {
-    const snapshot = await db.collection('notices').get();
-    snapshot.forEach((doc) => {
-        console.log(doc.id, '=>', doc.data());
-    });
-}
-const users = {
-    0: {
-        name: 'Kontum',
-        url: "https://lh3.googleusercontent.com/proxy/1_acZuOUd64lfI7NxGRkPgTawSjx6rLXgGCBrGt_TRVKm1-ztp4zYXSf5L20f6uCOOZ9y6RkNIAp5IiJ8VXdQjqqK6JU6XUayiUqGIGKsjr5I6xB-a-F"
-    },
-    1: {
-        name: 'Kai',
-        url: `https://sieupet.com/sites/default/files/gia-cho-shiba-inu-02-768x768_0.jpg`
-
-    },
-    2: {
-        name: 'Biden',
-        url: "https://image.thanhnien.vn/1024/uploaded/minhnguyet/2020_01_10/cho-pitbulll-4_nfps.jpg"
-
-    },
-    3: {
-        name: 'Win',
-        url: "https://upload.wikimedia.org/wikipedia/commons/b/b8/Degaen.jpg"
-    }
-}
-
 
 
 function loadMessages() {
 
-//    var query = await db.collection('messages').where("type","==","image").get()
-
+    //    var query = await db.collection('messages').where("type","==","image").get()
     var query = fireBaseAdmin.firestore()
         .collection('notices')
         .orderBy('timestamp', 'desc')
-        .limit(1);
+        .limit(3);
 
     // Start listening to the query.
+    var ref;
+    var message;
     query.onSnapshot(function (snapshot) {
         snapshot.docChanges().forEach(function (change) {
-            var message = change.doc.data();
+            message = change.doc.data();
+            message.doc_id = change.doc.id
             console.log(message);
         });
     });
-}
 
-async function sendMessage(message) {
-    try {
-        const idx = random(3)
-        await db.collection('notices').add({
-
-            name: users[`${idx}`].name,
-            text: users[`${idx}`].name,
-            profilePicUrl: users[`${idx}`].url,
-            timestamp: fireBaseAdmin.firestore.FieldValue.serverTimestamp()
-        })
-        console.log('send message', Date.now());
-    } catch (error) {
-        console.error('Error writing new message to database', error);
-    };
 
 
 }
 
-function loopSendMessage() {
-    setInterval(async () => {
-        await sendMessage(Date.now())
-    }, 2000);
+function updateMessage(docId) {
+    db.collection('notices').doc(id).update({ isView: true })
 }
+
+
+// async function sendMessage(message) {
+//     try {
+//         const idx = random(3)
+//         await db.collection('notices').add({
+
+//             name: users[`${idx}`].name,
+//             text: users[`${idx}`].name,
+//             profilePicUrl: users[`${idx}`].url,
+//             timestamp: fireBaseAdmin.firestore.FieldValue.serverTimestamp()
+//         })
+//         console.log('send message', Date.now());
+//     } catch (error) {
+//         console.error('Error writing new message to database', error);
+//     };
+
+
+// }
+
+// function loopSendMessage() {
+//     setInterval(async () => {
+//         await sendMessage(Date.now())
+//     }, 2000);
+// }
 
 async function start() {
     loadMessages()
-    loopSendMessage()
+    //  loopSendMessage()
 
 }
 
